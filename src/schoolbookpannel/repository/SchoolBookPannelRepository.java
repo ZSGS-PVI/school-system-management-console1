467a4cb42f1f280fc0df1d8337ac358154782304
kalpana@77 <102209171+kalpanadev22@users.noreply.github.com>
2023-12-20 15:50:14 +0530

    Update SchoolBookPannelRepository.java

--- src/schoolbookpannel/repository/SchoolBookPannelRepository.java
+++ src/schoolbookpannel/repository/SchoolBookPannelRepository.java
@@ -9,65 +9,36 @@ import schoolbookpannel.dto.Student;
 import schoolbookpannel.student.AddStudent;
 
 public class SchoolBookPannelRepository {
-	
+
 	private static SchoolBookPannelRepository repository;
-	
-	private Stack<Student> studentList=new Stack<Student>();
-	//private List<String> studentNames=new ArrayList<>();
+
+	private List<Student> studentList = new ArrayList<Student>();
+
 	private SchoolBookPannelRepository() {
-		
-		
+
 	}
+
 	public static SchoolBookPannelRepository getInstanse() {
-		if(repository==null) {
-			repository=new SchoolBookPannelRepository();
+		if (repository == null) {
+			repository = new SchoolBookPannelRepository();
 		}
 		return repository;
 	}
-	//1.add
+
+	// 1.add
 	public void insertStudent(Student Student) {
-		studentList.push(Student);
+		studentList.add(Student);
 	}
-	//2.view
-	public Stack<Student> getStudents() {
 
-		    return studentList;
+	// 2.view
+	public List<Student> getStudents() {
+
+		return studentList;
 	}
-	
 
-	public void setStudents(Stack<Student> updatedStudents) {
-        this.studentList = updatedStudents;
-        // You might also update a database or file with the updated list here
-    }
+	public void setStudents(List<Student> updatedStudents) {
+		this.studentList = updatedStudents;
 
-//	
-//	Stack<Student> copyStudentList = new Stack<>();
-//  copyStudentList.addAll(studentList);
-	
+	}
 
 }
-
-//public Stack<Student> updateStudent(Student updatedStudent) {
-//for(Student student11:studentList) {
-//	if(student11.getName().equalsIgnoreCase(updatedStudent.getName())) {
-//	if(student11.getDOB().equalsIgnoreCase(updatedStudent.getDOB())) {
-//
-//	if(student11.getMobileno().equalsIgnoreCase(updatedStudent.getMobileno())) {
-//
-//	if(student11.getEmail().equalsIgnoreCase(updatedStudent.getEmail())) {
-//
-////    System.out.println("Updated Student Details:");
-////    System.out.println("Name: "+student11.getName());
-////    System.out.println("DOB: "+student11.getDOB());
-////    System.out.println("Mobileno: "+student11.getMobileno());
-////    System.out.println("Eamil: "+student11.getEmail());
-//    break;
-//}
-//}
-//}			
-//}
-//}
-//  
-//return studentList;
-//}
-//
