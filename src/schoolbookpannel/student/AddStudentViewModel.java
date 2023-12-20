a92867c63bab21a6a8bd61c78724c8b5d462ecfe
kalpana@77 <102209171+kalpanadev22@users.noreply.github.com>
2023-12-20 15:49:15 +0530

    Update AddStudentViewModel.java

--- src/schoolbookpannel/student/AddStudentViewModel.java
+++ src/schoolbookpannel/student/AddStudentViewModel.java
@@ -1,5 +1,10 @@
 package schoolbookpannel.student;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -7,139 +12,197 @@ import schoolbookpannel.dto.Student;
 import schoolbookpannel.repository.SchoolBookPannelRepository;
 
 public class AddStudentViewModel {
-	
+
 	private AddStudent addStudent;
-//	
-//	public AddStudentViewModel(Student student) {
-//		this.student=student;
-//		
-//	}
-	
 
-    public AddStudentViewModel(AddStudent addStudent) {
-    	this.addStudent=addStudent;
+	public AddStudentViewModel(AddStudent addStudent) {
+		this.addStudent = addStudent;
 	}
 
-// 1.add user details
-   
+// 1.add student details
+
 	public void validate(Student student) {
-        
-            SchoolBookPannelRepository.getInstanse().insertStudent(student);
-            this.addStudent.onSuccess();
-       
-    
+
+		SchoolBookPannelRepository.getInstanse().insertStudent(student);
+		this.addStudent.onSuccess();
+
 	}
-	
-	
-// 2.view user details
-	
+
+// 2.view student details
+
 	public void viewUsers() {
-	    Stack<Student> students = SchoolBookPannelRepository.getInstanse().getStudents();
-	    
-	    if (students.isEmpty()) {
-	        System.out.println("No students found.");
-	    } else {
-	        System.out.println("List of students:");
-	        for (Student student : students) {
-	            // Process or display student information
-	        	this.addStudent.studentlist(student);
+		List<Student> students = SchoolBookPannelRepository.getInstanse().getStudents();
 
-	        }
-	    }
+		if (students.isEmpty()) {
+			System.out.println("No students found.");
+		} else {
+			System.out.println("List of students:");
+			for (Student student : students) {
+
+				this.addStudent.studentlist(student);
+
+			}
+		}
 	}
 
-	
-	public void update(String nameToUpdate) {
-		Stack<Student> students=SchoolBookPannelRepository.getInstanse().getStudents();
-	    
-		boolean found=false;
-		for(Student student:students) {
-			
-		    System.out.println(student);
-				found=true;
-				
-				Scanner in=new Scanner(System.in);
-				
-				System.out.println("1.name\n 2.DOB \n 3.mobileno\n 4.email \n5.back");
-				int choice=in.nextInt();
-				 in.nextLine();
-					
-				switch(choice) {
-				
-				case 1:  System.out.println("Enter the name: ");
-				         String newName=in.nextLine();
-				         student.setName(newName);
-				         break;
-				         
-				case 2:	 System.out.println("Enter the DOB: ");
-				         String newDOB=in.nextLine();
-				         student.setDOB( newDOB);
-			                 break;
-			             
-				case 3:   System.out.println("Enter the MobileNo: ");
-				          String newMobileNo=in.nextLine();
-                                          student.setMobileno(newMobileNo);
-                                         break;
-						
-				case 4:	System.out.println("Enter the email: ");
-				        String newEmail=in.nextLine();
-				        student.setEmail(newEmail);
+//3.Update the student details
+	public void update(int idToUpdate) {
+
+		List<Student> students = SchoolBookPannelRepository.getInstanse().getStudents();
+
+		boolean found = false;
+		for (Student student : students) {
+
+			if (student.getRollNo() == (idToUpdate)) {
+
+				found = true;
+
+				Scanner in = new Scanner(System.in);
+
+				System.out.println(" 1.Name\n 2.DOB \n 3.Mobileno\n 4.Email\n 5.Back");
+				int choice = in.nextInt();
+				in.nextLine();
+
+				switch (choice) {
+
+				case 1:
+					System.out.println("Enter  new name: ");
+					String newName = in.nextLine();
+					student.setName(newName);
 					break;
-				
-	                      default: System.out.println("Invalid choice.");
-	                                break;
+
+				case 2:
+					System.out.println("Enter   new DOB: ");
+					String newDOB = in.nextLine();
+					student.setDOB(newDOB);
+					break;
+
+				case 3:
+					System.out.println("Enter  new MobileNo: ");
+					String newMobileNo = in.nextLine();
+					student.setMobileno(newMobileNo);
+					break;
+				case 4:
+					System.out.println("Enter new email: ");
+					String newEmail = in.nextLine();
+
+					student.setEmail(newEmail);
+					break;
+
+				default:
+					System.out.println("Invalid choice.");
+					break;
+
 				}
-					
-					
+
 				System.out.println("Student details updated successfully.");
+				System.out.println();
 				break;
 			}
-		
-		
-		  if (!found) {
-		        System.out.println("Student not found.");
-		    }
-		
+		}
+
+		if (!found) {
+			System.out.println("Student not found with ID: " + idToUpdate);
+		}
+	}
+
+// 4.Delete the student Details
+	public void delete(int idTODelete) {
+		List<Student> students = SchoolBookPannelRepository.getInstanse().getStudents();
+		boolean found = false;
+
+		Iterator<Student> iterator = students.iterator();
+		while (iterator.hasNext()) {
+			Student student = iterator.next();
+			if (student.getRollNo() == idTODelete) {
+				iterator.remove();
+				found = true;
+				System.out.println("Student deleted successfully");
+				break;
+			}
+		}
+
+		if (!found) {
+			System.out.println("Student not found.");
+		}
+
+		SchoolBookPannelRepository.getInstanse().setStudents(students);
 	}
 	
-	
-	
-	public void delete(String nameToDelete) {
-	    Stack<Student> students = SchoolBookPannelRepository.getInstanse().getStudents();
-	    boolean found = false;
-        
-	    Iterator<Student> iterator=students.iterator();
-	    while(iterator.hasNext()) {
-	    	Student student=iterator.next();
-	    	if(student.getName().equalsIgnoreCase(nameToDelete)) {
-	    		iterator.remove();
-	    		found=true;
-	    		System.out.println("Student deleted successfully");
-	    		break;
-	    	}
-	    }
 
-	    if (!found) {
-	        System.out.println("Student not found.");
-	    }
 
-	  
-	    SchoolBookPannelRepository.getInstanse().setStudents(students);
+	// student name validation
+	public void isValidName(String name) {
+
+		if (name.matches("^[a-zA-Z]+$")) {
+		}
+
+		else {
+			System.err.println("only characters are allowed!");
+			addStudent.addStudentInfo();
+		}
+
+	}
+
+	// student DOB validation
+	public void isValidDateOfBirth(String dateStr) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		dateFormat.setLenient(false);
+
+		try {
+			Date date = dateFormat.parse(dateStr);
+
+			Date currentDate = new Date();
+
+			if (!date.after(currentDate) && date.before(currentDate)) {
+
+			}
+		} catch (ParseException e) {
+			// ParseException will be thrown if the date format is invalid
+			System.err.println("Invalid date of birth!");
+			addStudent.addStudentInfo();
+
+		}
+	}
+
+	// student Mobile number validation
+
+	public void isValidMobileNo(String number) {
+
+		if (number != null && !number.isEmpty()) {
+
+			if (number.matches("^[0-9]{10}$")) {
+
+			} else {
+				System.err.println("Invalid mobile number! Only 10 digits are allowed.");
+				addStudent.addStudentInfo();
+			}
+		} else {
+			System.err.println("Mobile number cannot be empty or null!");
+			addStudent.addStudentInfo();
+		}
+	}
+
+	// student Email validation
+	public void isValidEmail(String name) {
+
+		if (name != null && !name.isEmpty()) {
+
+			if (name.matches(
+					"^[A-Za-z0-9-\\+]+(\\.[A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")) {
+
+			}
+
+			else {
+				System.err.println("Invalid Email Id!");
+				addStudent.addStudentInfo();
+			}
+
+		} else {
+			System.err.println("Email Id cannot be empty or null!");
+			addStudent.addStudentInfo();
+		}
+
 	}
 
 }
-	
-
-
-
-
-
-
-	      
-
-
-
-
-
-
-		  
