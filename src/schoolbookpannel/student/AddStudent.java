2e5871243b7a19d3afa9dfc352dd8edcd9c9ed2d
kalpana@77 <102209171+kalpanadev22@users.noreply.github.com>
2023-12-20 15:48:00 +0530

    Update AddStudent.java

--- src/schoolbookpannel/student/AddStudent.java
+++ src/schoolbookpannel/student/AddStudent.java
@@ -3,85 +3,90 @@ package schoolbookpannel.student;
 import java.util.List;
 import java.util.Scanner;
 
-
 import schoolbookpannel.dto.Student;
 
 public class AddStudent {
-	private static final String String = null;
+	// private static final String String = null;
 	private AddStudentViewModel addStudentviewmodel;
-	 
+
 	public AddStudent() {
-		addStudentviewmodel=new AddStudentViewModel(this);
+		addStudentviewmodel = new AddStudentViewModel(this);
 	}
-	
-	
-	public void nam() {
-		 Scanner in = new Scanner(System.in);
-	        String name;
-	        String DOB;
-	        String mobileno;
-	        String email;
-	      
-	      
 
-	        while (true) {
-	            System.out.println("Enter a student name  (type 'exit' to stop adding names):");
-	            name = in.nextLine();
-	            if (name.equalsIgnoreCase("exit")) {
-	                break;
-	            }
-	            
-	            System.out.println("Enter a student DOB : ");
-	           DOB = in.nextLine();
-	           System.out.println("Enter a student mobileno: ");
-	           mobileno = in.nextLine();
-	           System.out.println("Enter a student email  ");
-	          email = in.nextLine();
-	           
-	            
-	            
+	Scanner in = new Scanner(System.in);
 
-	           
-	            Student student=new Student(name,DOB,mobileno,email);
-	        	addStudentviewmodel.validate(student);
-	           
+	public void addStudentInfo() {
+		Scanner in = new Scanner(System.in);
+		int rollNo = 0;
+		String name;
+		String DOB;
+		String mobileno;
+		String email;
+
+		while (true) {
+			System.out.println("Enter the rollNo  (type '0' to stop adding names):");
+			rollNo = in.nextInt();
+			if (rollNo == 0) {
+				break;
+			}
+
+			System.out.println("Enter  student name:");
+			name = in.next();
+			addStudentviewmodel.isValidName(name);
+
+			System.out.println("Enter student DOB Like (\"yyyy-MM-dd\"): ");
+			DOB = in.next();
+			addStudentviewmodel.isValidDateOfBirth(DOB);
+			System.out.println("Enter  student mobileno: ");
+			mobileno = in.next();
+			addStudentviewmodel.isValidMobileNo(mobileno);
+
+			System.out.println("Enter student email  ");
+			email = in.next();
+			addStudentviewmodel.isValidEmail(email);
+
+			Student student = new Student(rollNo, name, DOB, mobileno, email);
+			addStudentviewmodel.validate(student);
+
+		}
 	}
-	}
-	
 
-	public void viewUsers()  {
+	public void viewStudent() {
 		addStudentviewmodel.viewUsers();
 	}
-	
-	public void updateUsers()  {
-		addStudentviewmodel.update(String);
+
+	public void updateStudent() {
+
+		System.out.println("Enter the Id to update:");
+		int idTOUpdate = in.nextInt();
+		addStudentviewmodel.update(idTOUpdate);
 	}
-	
-	public void deleteUsers()  {
-		Scanner in=new Scanner(System.in);
-		System.out.println("Enter the name of the student to delete:");
-		String nameToDelete = in.nextLine();
-		 
-		addStudentviewmodel.delete(nameToDelete);
+
+	public void deleteStudent() {
+
+		System.out.println("Enter the Id to delete:");
+		int idTODelete = in.nextInt();
+
+		addStudentviewmodel.delete(idTODelete);
 	}
+
 	public void onSuccess() {
 		System.out.println("Inserted Successfully");
 
 	}
-	
 
 	public void showError(String errorMessage) {
 		System.out.println(errorMessage);
 	}
-   public void studentlist(Student student) {
-	   System.out.println("---------------------------");
-       System.out.println("Name: " + student.getName());
-       System.out.println("DOB: " + student.getDOB());
-       System.out.println("Mobile No: " + student.getMobileno());
-       System.out.println("Email: " + student.getEmail());
-       System.out.println("---------------------------");
-   }
 
-
+	public void studentlist(Student student) {
+		System.out.println("---------------------------");
+		System.out.println("ID: " + student.getRollNo());
+		System.out.println("Name: " + student.getName());
+		System.out.println("DOB: " + student.getDOB());
+		System.out.println("Mobile No: " + student.getMobileno());
+		System.out.println("Email: " + student.getEmail());
+		System.out.println("---------------------------");
+	}
 
 }
